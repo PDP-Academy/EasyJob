bd82e9a04919d28261647643b308fa00b796c3da
KhAbdukhoshimov <Khangitlab2710>
2023-01-13 11:33:45 +0500

    Added custom exceptions for user services

---
+++ src/EasyJob.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyJob.Domain.Exceptions;
+
+public class NotFoundException : Exception
+{
+	public NotFoundException(string message)
+		: base(message)
+	{
+	}
+}
---
+++ src/EasyJob.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyJob.Domain.Exceptions;
+
+public class ValidationException : Exception
+{
+	public ValidationException(string message)
+		: base(message)
+	{
+	}
+}
