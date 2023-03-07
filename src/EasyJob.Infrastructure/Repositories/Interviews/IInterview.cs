234e03d7e0a16bed8025d96e39941425b5d3b0e2
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-03-07 20:39:50 +0500

    InterviewRepo has been added

---
+++ src/EasyJob.Infrastructure/Repositories/Interviews/IInterview.cs
@@ -0,0 +1,6 @@
+﻿namespace EasyJob.Infrastructure.Repositories.Interviews;
+
+public interface IInterview : IGenericRepository<Interview, Guid>
+{
+
+}
---
+++ src/EasyJob.Infrastructure/Repositories/Interviews/Interview.cs
@@ -0,0 +1,11 @@
+﻿using EasyJob.Infrastructure.Contexts;
+
+namespace EasyJob.Infrastructure.Repositories.Interviews;
+
+public class Interview : GenericRepository<Interview, Guid>, IInterview
+{
+	public Interview(AppDbContext appDbContext)
+		: base(appDbContext)
+	{
+	}
+}
