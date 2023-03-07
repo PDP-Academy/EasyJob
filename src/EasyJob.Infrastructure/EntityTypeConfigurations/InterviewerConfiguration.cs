be7e5931f0a0874dc1c3b25973d3b851038fc133
eJwtzLFuwjAUheF38ZzBpTEk3pAISK3YWlbLvrkOTkxsXSeFCPHutVC2oyN9/5NpAEzpJww4MsmwKgWWnIM1G1ObrS2x4ry29QevrBafOy3AoAVWsEjhz7VIK32uJTWtqe4aFGzT7wX1Tojef6XmSOezE0PHm8P+u7+Up6aHJafeRE1LxOwMakLKb4LwPnR7c6OMs/EO1IBLQahbGajLI4ZiTkgSb9r54h5osD7cM8ZHdIRJ6YnJcfb+9foHstpQgg== <axadovravshan@mail.ru>
2023-03-07 20:29:19 +0500

    interviewerConfiguration

--- src/EasyJob.Infrastructure/EntityTypeConfigurations/InterviewerConfiguration.cs
+++ src/EasyJob.Infrastructure/EntityTypeConfigurations/InterviewerConfiguration.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EasyJob.Domain.Constants;
+using EasyJob.Domain.Entities.Interviews;
+using EasyJob.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace EasyJob.Infrastructure.EntityTypeConfigurations
+namespace EasyJob.Infrastructure.EntityTypeConfigurations;
+
+public class InterviewerConfiguration : IEntityTypeConfiguration<Interviewer>
 {
-    internal class InterviewConfiguration
+    public void Configure(EntityTypeBuilder<Interviewer> builder)
     {
+        builder.ToTable(TableName.Interviewer);
+
+        builder
+            .Property(interviewer => interviewer.Description)
+            .IsRequired(true);
+
+        builder
+            .Property(interviewer => interviewer.Position)
+            .IsRequired(true);
+
+        builder
+            .Property(interviewer => interviewer.Speciality)
+            .IsRequired(true);
+
+        builder
+            .Property(interviewer => interviewer.Company)
+            .IsRequired(false);
+
+        builder
+            .HasOne(interviewer => interviewer.User)
+            .WithOne()
+            .HasForeignKey<User>(user => user.Id);
+
+        builder
+            .HasMany(interviewer => interviewer.Skills)
+            .WithOne()
+            .HasForeignKey(skill => skill.Id);
+
     }
 }
