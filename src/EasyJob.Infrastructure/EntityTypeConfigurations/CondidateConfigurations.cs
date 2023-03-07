ca37149ff03df981eef28bad1afeae463cb8cfbd
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-03-07 20:03:51 +0500

    Add condidate configuration

---
+++ src/EasyJob.Infrastructure/EntityTypeConfigurations/CondidateConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using EasyJob.Domain.Constants;
+using EasyJob.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyJob.Infrastructure.EntityTypeConfigurations;
+
+public class CondidateConfigurations : IEntityTypeConfiguration<Candidate>
+{
+    public void Configure(EntityTypeBuilder<Candidate> builder)
+    {
+        builder.ToTable(TableName.Condidate);
+
+        builder.Property(condidate => condidate.ResumeUrl)
+            .IsRequired();
+
+        builder
+            .HasOne(condidate => condidate.User)
+            .WithOne()
+            .HasForeignKey<Candidate>(condidate => condidate.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
