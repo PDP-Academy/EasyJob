005448e90bddd1ac9714e7f9a2df1504d230b779
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-07 20:39:03 +0500

    fixed

--- src/EasyJob.Domain/Entities/Users/Admin.cs
+++ src/EasyJob.Domain/Entities/Users/Admin.cs
@@ -1,5 +1,8 @@
-﻿namespace EasyJob.Domain.Entities.Users;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace EasyJob.Domain.Entities.Users;
+
+[Keyless]
 public class Admin
 {
     public Guid UserId { get; }
