60b6819cc495de09e27c73a2466e65736dd24aeb
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-07 19:38:38 +0500

    Added Skill Model Configuration

---
+++ src/EasyJob.Infrastructure/EntityTypeConfigurations/SkillConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EasyJob.Domain.Constants;
+using EasyJob.Domain.Entities.Skills;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyJob.Infrastructure.EntityTypeConfigurations;
+
+public class SkillConfiguration : IEntityTypeConfiguration<Skill>
+{
+    public void Configure(EntityTypeBuilder<Skill> builder)
+    {
+        builder.ToTable(TableName.Skills);
+
+        builder.HasKey(skill => skill.Id);
+
+        builder.Property(skill => skill.Name)
+            .IsRequired(true)
+            .HasMaxLength(75);
+    }
+}
