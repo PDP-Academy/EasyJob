e502a70c30d2b53b2c0d97759e35f580b3280209
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-03-08 18:02:56 +0500

    Add CandidateRepository to Infrastructure layer

---
+++ src/EasyJob.Infrastructure/Repositories/Candidates/CandidateRepository.cs
@@ -0,0 +1,13 @@
+﻿using EasyJob.Domain.Entities.Users;
+using EasyJob.Infrastructure.Contexts;
+
+namespace EasyJob.Infrastructure.Repositories.Candidates;
+
+public sealed class CandidateRepository : GenericRepository<Candidate, Guid>
+    , ICandidateRepository
+{
+    public CandidateRepository(AppDbContext appDbContext)
+        : base(appDbContext)
+    {
+    }
+}
